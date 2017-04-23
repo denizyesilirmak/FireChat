e184f3c8ae45687745ca1352202dee82421b9319
unknown <denizyesilirmak@gmail.com>
2017-04-23 03:52:18 +0300

    One to one chat works.

--- js/app.js
+++ js/app.js
@@ -3,8 +3,56 @@ var app = angular.module('chatApp', ['firebase']);
 
 app.controller('mainController', function ($scope, $firebaseArray) {
     var me;
+    $scope.currentConversation = "---";
+
+    //User ref
     var refUsers = firebase.database().ref().child('Users');
     $scope.users = $firebaseArray(refUsers);
+    //Conversation ref
+    var refConversations = firebase.database().ref().child('Conversations');
+    $scope.conversations = $firebaseArray(refConversations);
+    //Messages ref
+    var refMessages = firebase.database().ref().child('Messages');
+    $scope.messages = $firebaseArray(refMessages);
+
+    $scope.createConversation = function (selectedUserID, userName) {
+        var isConversationExist = false;
+        $scope.conversations.forEach(function (a) {
+            console.log(JSON.stringify(a.userids));
+            console.log(JSON.stringify(b));
+            var b = [me.$id, selectedUserID];
+            var c = [selectedUserID, me.$id];
+            if (JSON.stringify(a.userids) === JSON.stringify(b) || JSON.stringify(a.userids) === JSON.stringify(c)) {
+                console.log('buldum');
+                isConversationExist = true;
+                console.log("var");
+                $scope.currentConversation = a.$id;
+            }
+        });
+        if (isConversationExist == false) {
+            console.log("yok");
+            $scope.conversations.$add({
+                userids: [
+                    selectedUserID, me.$id
+                ]
+                }).then(function(p){
+                    $scope.currentConversation = p['path']['o'][1];
+            });
+        }
+
+        $scope.reciever = userName;
+    }
+
+    $scope.sendMessage = function () {
+        $scope.messages.$add({
+            conversationID: $scope.currentConversation,
+            body : $scope.me.username + " : " + $scope.messageText,
+            date : Date.now(),
+            sender : $scope.me.$id
+        });
+
+        $scope.messageText = "";
+    }
 
     // login
     $scope.isLogined = true;
